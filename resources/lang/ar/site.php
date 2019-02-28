eef07202108348cbe66cffe80c4d6509f68e2583
AbdoAboabdo <abdoaboabdo84@gmail.com>
2019-02-28 14:55:25 +0200

    first commit

---
+++ resources/lang/ar/site.php
@@ -0,0 +1,66 @@
+<?php
+
+return [
+    'dashboard' => 'الرئيسيه',
+
+    'add' => 'اضف',
+    'create' => 'اضافه',
+    'read' => 'عرض',
+    'edit' => 'تعديل',
+    'update' => 'تعديل',
+    'delete' => 'حذف',
+    'search' => 'بحث',
+
+    'confirm_delete' => 'تاكيد الحذف',
+    'yes' => 'نعم',
+    'no' => 'لا',
+
+    'login' => 'تسجيل الدخول',
+    'remember_me' => 'تذكرني',
+    'password' => 'كلمه المرور',
+    'password_confirmation' => 'تاكيد كلمه المرور',
+
+    'added_successfully' => 'تم اضافه البيانات بنجاح',
+    'updated_successfully' => 'تم تعديل البيانات بنجاح',
+    'deleted_successfully' => 'تم حذف البيانات بنجاح',
+
+    'no_data_found' => 'للاسف لا يوجد اي سجلات',
+
+    'clients' => 'العملاء',
+    'phone' => 'التلفون',
+    'address' => 'العنوان',
+
+    'users' => 'المشرفين',
+    'first_name' => 'الاسم الاول',
+    'last_name' => 'الاسم الاخير',
+    'email' => 'البريد الاكتروني',
+    'image' => 'صوره',
+    'action' => 'اكشن',
+
+    'permissions' => 'الصلاحيات',
+
+    'categories' => 'الاقسام',
+    'all_categories' => 'كل الاقسام',
+    'name' => 'الاسم',
+    'description' => 'الوصف',
+    'products_count' => 'عدد الامنتجات',
+    'related_products' => 'المنتجات المرتبطه',
+    'category' => 'القسم',
+
+    'products' => 'المنتجات',
+    'purchase_price' => 'سعر الشراء',
+    'sale_price' => 'سعر البيع',
+    'stock' => 'المخزن',
+    'profit_percent' => 'المكسب',
+
+    'ar' => [
+        'name' => 'الاسم بالغه العربيه',
+        'description' => 'الوصف بالغه العربيه',
+    ],
+
+    'en' => [
+        'name' => 'الاسم بالغه الانجليزيه',
+        'description' => 'الوصف بالغه الانجليزيه',
+    ],
+
+];
---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.dashboard.app')
+
+@section('content')
+    <div class="content-wrapper">
+        <section class="content-header">
+            <h1>Blank Page
+                <small>it all start here</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li class="active breadcrumb-item"><i class="fa fa-dashboard">@lang('site.dashboard')</i></li>
+            </ol>
+        </section>
+        <section class="content">
+            <h1>this id dashboard</h1>
+
+        </section>
+    </div>
+@endsection
